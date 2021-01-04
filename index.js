7f5a5ad0f9c8ef984f25b3e3a355ac754809616f
murathan karayazi <murathan.karayazi@adcolony.com>
2021-01-04 10:46:16 +0300

    Rest API Trial with ExpressJS

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ import projectRoutes from "./routes/projects.js";
 
 app.use(bodyParser.json());
 
+// app.use() also is where we can call middleware functions.
 app.use("/projects", projectRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Home route. You can make request to '/projects' route for more.");
 });
 
 app.listen(3030, () => console.log("Server is running at", 3030));
--- routes/projects.js
+++ routes/projects.js
@@ -1,14 +1,76 @@
 import express from "express";
 
-import dummy from "../dummy.json";
-
 const router = express.Router();
 
+let projects = [
+  {
+    id: "1",
+    name: "Project 1",
+    author: "Author 1",
+    coAuthor: "Co-Author 1",
+    createdAt: "ISO DATE",
+    updatedAt: "ISO DATE",
+  },
+  {
+    id: "2",
+    name: "Project 2",
+    author: "Author 2",
+    coAuthor: "Co-Author 2",
+    createdAt: "ISO DATE",
+    updatedAt: "ISO DATE",
+  },
+];
+
 router.get("/", (req, res) => {
-  console.log("dum", dummy);
-  res.send(
-    "<div style='background-color: grey; width: 200; height: 100; color: red;'>Hello world</div>"
-  );
+  console.log("Getting all projects.");
+  res.send(projects);
+});
+
+//get with ID
+router.get("/:id", (req, res) => {
+  console.log("Getting the project with id ", id);
+  const { id } = req.params;
+  const project = projects.find((e) => e.id == id);
+  res.send(project ? project : "Couldn't found");
+});
+
+router.post("/", (req, res) => {
+  const project = req.body;
+  console.log("Creating project", project);
+  if (project.id && !projects.find((e) => e.id == project.id)) {
+    project.createdAt = new Date().toISOString();
+    projects.push(project);
+    res.send(`Project ${project.name} created with id ${project.id}`);
+  } else {
+    res.send(
+      "Creating project failed. Please check your Project ID and Project Name."
+    );
+  }
+});
+
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, author, coAuthor } = req.body;
+  const project = projects.find((e) => e.id == id);
+  if (project) {
+    if (name) project.name = name;
+    if (author) project.author = author;
+    if (coAuthor) project.coAuthor = coAuthor;
+    project.updatedAt = new Date().toISOString();
+    res.send(`Project with id ${id} has been updated.`);
+  } else {
+    res.send("Project cannot be found");
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  if (id) {
+    projects = projects.filter((e) => e.id !== id);
+    res.send(`Project deleted with id ${id}`);
+  } else {
+    res.send("Project cannot be found.");
+  }
 });
 
 export default router;
